fb9d1b48a8759436ce37e55f832cc07e65c548d4
Tom Xue <tomxue0126@gmail.com>
2012-07-31 13:08:08 +0800

    to show what is Java's interface and its multiple inheritance

---
+++ InterfaceTest.java
@@ -0,0 +1,37 @@
+public class InterfaceTest {
+    interface  InterfaceA {
+         int  len =  1 ;
+         void  output();
+    }
+
+    interface  InterfaceB {
+           int  len =  2 ;
+           void  output();
+    }
+
+    interface  InterfaceSub  extends  InterfaceA, InterfaceB {            }
+
+    public static class Xyz implements  InterfaceSub {
+
+         public   void  output() {
+            System.out.println( "output in class Xyz." );
+        }
+
+          public   void  outputLen(int  type) {
+              switch (type) {
+                      case  InterfaceA.len:
+                             System.out.println( "len of InterfaceA=." +type);
+                              break ;
+                      case  InterfaceB.len:
+                             System.out.println( "len of InterfaceB=." +type);
+                              break ;
+             }
+        }
+    }
+            
+    public   static   void  main(String[] args) {
+           Xyz xyz = new Xyz();
+           xyz.output();
+           xyz.outputLen(1);
+   }
+}
